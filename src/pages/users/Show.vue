b4a553b32bbe51a7c8f50c9cf0c8823a2deb95f3
MohsenSami <73436220+mohsensami@users.noreply.github.com>
2022-07-20 17:02:44 +0430

    add posts list for user

--- src/pages/users/Show.vue
+++ src/pages/users/Show.vue
@@ -6,6 +6,19 @@
   <div v-else class="col-md-4">
     <UserCardViwe :user="user" />
   </div>
+  <div class="col-md-8">
+    <h4>Posts</h4>
+    <hr>
+    <ul v-for="post in posts">
+      <li class="d-flex">
+        <router-link
+          :to="{ name: 'postId', params: { id: post.id } }"
+          >{{ post.title }}</router-link
+        >
+
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -20,6 +33,7 @@ export default {
   },
   setup() {
     const user = ref({});
+    const posts = ref({});
     const loading = ref(true);
     const route = useRoute();
 
@@ -34,10 +48,22 @@ export default {
           console.log(error);
         });
     }
-
     getUser();
+    
+    function getPosts() {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts?userId=${route.params.id}`)
+        .then(function (response) {
+          posts.value = response.data;
+          loading.value = false;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    getPosts();
 
-    return { user, loading };
+    return { user, loading, posts };
   },
 };
 </script>
